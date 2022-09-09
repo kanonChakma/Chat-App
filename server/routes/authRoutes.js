0a87827a9b7d755e3fa78345b687b3c45ab3d392
kanon chakma <kanonchakma@pop-os.localdomain>
2022-09-10 01:22:16 +0600

    feat: add message fucntionality

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
-import { login, register, setAvatar } from '../controllers/user.js';
+import { getAllUsers, login, register, setAvatar } from '../controllers/user.js';
 
 const router = express.Router()
 router.post("/register", register);
 router.post("/login", login);
 router.post("/setavatar/:id", setAvatar)
+router.get("/allusers/:id", getAllUsers);
 export default router;
